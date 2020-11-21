6e59021b52aca0024f5cab46290797ca04a9f097
이승윤 <yyuunn17@naver.com>
2020-11-21 22:50:34 +0900

    modify: test code
    max error 33

--- DanaJJang/src/main/java/com/github/syxxn/DanaJJang/entity/folder/Folder.java
+++ DanaJJang/src/main/java/com/github/syxxn/DanaJJang/entity/folder/Folder.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Entity
 @Builder
 @Getter
-@Table(name="Folder")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Folder {
--- DanaJJang/src/main/java/com/github/syxxn/DanaJJang/service/user/UserServiceImpl.java
+++ DanaJJang/src/main/java/com/github/syxxn/DanaJJang/service/user/UserServiceImpl.java
@@ -5,16 +5,20 @@ import com.github.syxxn.DanaJJang.entity.user.UserRepository;
 import com.github.syxxn.DanaJJang.exception.UserAlreadyExistsException;
 import com.github.syxxn.DanaJJang.payload.request.SignUpRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
+
     public final UserRepository userRepository;
+    public final PasswordEncoder passwordEncoder;
 
     @Override
     public void signUp(SignUpRequest signUpRequest){
-        userRepository.findByUserId(signUpRequest.getUserId()).ifPresent(user->{
+        userRepository.findByUserId(signUpRequest.getUserId())
+                .ifPresent(user->{
                  throw new UserAlreadyExistsException();
         });
         userRepository.save(
