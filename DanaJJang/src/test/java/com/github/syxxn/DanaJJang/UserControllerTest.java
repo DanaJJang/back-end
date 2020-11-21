36fe788fd002af2c2b38f6251e4d74c852a55208
이승윤 <yyuunn17@naver.com>
2020-11-21 13:57:56 +0900

    add: error

--- DanaJJang/src/test/java/com/github/syxxn/DanaJJang/UserControllerTest.java
+++ DanaJJang/src/test/java/com/github/syxxn/DanaJJang/UserControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles({"test", "local"})
 public class UserControllerTest {
 
     @Autowired
